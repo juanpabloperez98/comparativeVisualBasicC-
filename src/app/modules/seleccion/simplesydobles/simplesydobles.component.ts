10491f767301cc7efb6b3a627f9dd7c825a994c3
juanpabloperez98 <juanpabloperezdevelopment@gmail.com>
2023-04-28 22:02:49 -0500

    Adding simplesdobles.component.ts

--- src/app/modules/seleccion/simplesydobles/simplesydobles.component.ts
+++ src/app/modules/seleccion/simplesydobles/simplesydobles.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HighlightService } from 'src/app/services/highlight.service';
+
 
 @Component({
   selector: 'app-simplesydobles',
@@ -6,6 +8,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./simplesydobles.component.scss']
 })
 export class SimplesydoblesComponent implements OnInit {
+
+  CODECSHARP1 = ` namespace Identificadores
+  {
+      class Program
+      {
+          static void Main(string[] args)
+          {
+              tipo_dato identificador_variable;
+              tipo_dato identificador_funcion(tipo_dato){ }
+              tipo_dato identificador_constante = 1;
+          }
+      }
+  }
+  `
+  CODECVB1 = ` Module Program
+  Sub Main()
+      Dim identificador_variable As tipo_dato
+      Const identificador_constante As tipo_dato
+  End Sub
+End Module
+  `
+
+  // ejemplo 2
+  CODECSHARP2 = ` namespace Identificadores
+  {
+      class Program
+      {
+          static void Main(string[] args)
+          {
+              tipo_dato identificador_variable;
+              tipo_dato identificador_funcion(tipo_dato){ }
+              tipo_dato identificador_constante = 1;
+          }
+      }
+  }
+  `
+  CODECVB2 = ` Module Program
+  Sub Main()
+      Dim identificador_variable As tipo_dato
+      Const identificador_constante As tipo_dato
+  End Sub
+End Module
+  `
 
   constructor() { }
 
