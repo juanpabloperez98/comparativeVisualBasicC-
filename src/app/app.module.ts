0e9da9f9148825cfcdee51732f993cfa50873604
juanpabloperez98 <juanpabloperezdevelopment@gmail.com>
2023-07-05 11:41:51 -0500

    Adding manuales path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainNavbarComponent } from './shared/main-navbar/main-navbar.component';
-import { CardComponent } from './shared/card/card.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { ErrorpageComponent } from './shared/errorpage/errorpage.component';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
 import { AppRoutes } from './app-routing.module';
@@ -18,9 +15,6 @@ import { HighlightService } from './services/highlight.service';
   declarations: [
     AppComponent,
     MainNavbarComponent,
-    CardComponent,
-    FooterComponent,
-    ErrorpageComponent,
     HomeComponent,
   ],
   imports: [
